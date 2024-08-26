93fe5bd060b5e8e31123bba0e6a1587dd0c9ea66
NANDHININAREN <727722euec110@skcet.ac.in>
2024-08-26 23:07:02 +0530

    initial commit

--- js/crosswordPuzzle.js
+++ js/crosswordPuzzle.js
@@ -1,26 +1,4 @@
-/**
- * MIT License
 
-Copyright (c) 2017 deepakshajan
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
- */
 
 "use strict";
 
@@ -116,62 +94,62 @@ var cwp_nmsp = {};
 	namespace.CWP_NO_COLS = 20;
 
 	namespace.CWP_SOLUTION_ARRAY = [[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	 'R',	 'H',	 'I',	 'N',	 'O',	 'C',    'E',    'R',    'O',    'S',   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,    'L',   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	 'G',	 'I',	 'R',	 'A',	 'F',	 'F',    'E',   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,    'P',   null,   null,   null,   null,    'H',   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	 'M',	null,	null,	null,    'H',   null,   null,   null,    'D',    'O',    'G',   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	 'O',	null,	null,	null,    'A',   null,   null,    'C',   null,    'R',   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	 'P',	 'E',	 'N',	 'G',	 'U',	 'I',    'N',   null,   null,    'A',   null,    'S',   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	 'K',	null,	null,	null,    'T',    'U',    'R',    'T',    'L',    'E',   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	 'Z',	 'E',	 'B',	 'R',	 'A',   null,   null,    'A',   null,    'I',   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	 'Y',	null,	null,	null,   null,   null,    'B',   null,    'O',   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,    'B',   null,    'N',   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,    'I',   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	 'A',	 'L',	 'L',	 'I',    'G',    'A',    'T',    'O',    'R',   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]];
-
+	                                [null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   'S',    null,   null],
+                                   	[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   'I',    null,   null],
+                                   	[null,	null,	null,	null,   'S',	'P',	'E',	'E',	'C',	'H',    null,   null,   null,   null,   null,   null,   null,   'L',    null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	null,   'E',    null,   null,   null,   null,   null,   null,   null,   'E',    null,   null],
+									[null,	null,	null,	'A',	'G',	'R',	'I',	'C',	'U',    'L',    'T',    'U',    'R',    'E',    null,   null,   null,   'N',    null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	null,   'E',    null,   null,   null,   null,   null,   null,   null,   'C',    null,   null],
+									[null,	null,	null,	'P',	null,	null,	null,	null,	null,   'V',    null,   'R',    'E',    'M',    'E',    'D',    'I',    'E',    'S',    null],
+									[null,	null,	'P',	'R',	'E',	'A',	'M',	'B',	'L',    'E',    null,   null,   null,    null,   null,  null,   null,   null,   null,   null],
+									[null,	null,	null,	'O',	null,	null,	null,	null,   null,   'N',    null,   null,   'A',    'M',    'B',    'E',    'D',    'K',    'A',     'R'],
+									[null,	null,	null,	'T',	null,	null,	null,	null,	null,   null,   null,   'H',    null,    null,   null,  null,   null,   null,   null,   null],
+									[null,	null,	null,	'E',	null,	null,	null,	null,   null,   'P',    'E',    'A',    'C',    'E',    null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	'C',	null,	null,	null,	null,	null,   null,   'Q',    'R',    null,   'I',    null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	'T',	null,   null,	null,	null,	null,   null,   'U',    'M',    null,   'G',    null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   'A',    'O',    null,   'H',    null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   'L',    'N',    null,   'T',    null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   'I',    'Y',    null,   'H',    null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   'T',    null,   null,   null,   null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	'P',    'A',    'Y',    null,   null,   null,   null,   null,   null,   null,   null,   null],
+									[null,	null,	null,	null,	null,	null,	null,	null,	null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]];
 
 	namespace.CWP_Q_INDEX_ARRAY  = [[null,  null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,      1,   null,   null,   null,   null,   null,      2,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,      3,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,     10,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,      5,   null,   null,   null,   null,   null,   null,   null,      6,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,      7,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,    '8',   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,      9,   null,      4,   null,     11,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,     12,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,     13,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null],
-	                                [null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null]];
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,    13,   null,   null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,   null,   null,      1,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,      2,   null,   null,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,   null,      3,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,   null,      5,   null,   null,   null,   null,   null,   null,   null,      6,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,      8,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null,   null,   null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,     11,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,      4,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,      9,     10,   null,   null,     12,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,      7,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null],
+									[null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,   null,  null]];
 
-	namespace.CWP_QUESTIONS_ACROSS = [		"1. Big animal that has one horn",
-	                                  		"3. This animal has a very long neck",
-	                                  		"6. Man's best friend",
-	                                  		"8. Lives in the cold and waddles",
-	                                  		"9. Slow moving and has a shell",
-	                                  		"12. Striped animal that looks like a horse",
-	                                  		"13. Can swim under water and walk on land and has a big bite"];
 
-	namespace.CWP_QUESTIONS_DOWN =   [      "2. This animal has a long trunk",
-	                                        "4. This animal likes to eat carrots and sugar cuubes",
-	                                        "5. This animal loves bananas",
-	                                        "7. Some say this anmal has nine lives",
-	                                        "10. Has big ears and likes to hop",
-	                                        "11. King of the jungle"];
+	namespace.CWP_QUESTIONS_ACROSS = [	    "1. Which Article guarantees the 'Right to Freedom of Speech and Expression'?",
+											"3. Which Directive Principle deals with the 'Organization of Agriculture and Animal husbandry'?",
+											"6. Which Fundamental Right is often referred to as the 'Heart and Soul' of the Indian Constitution?",
+											"7. Which Article advocates for 'Equal pay for equal work' for men and women?",
+											"8. What term is used for the introduction to the Constitution, which states the objectives of the Constitution?",
+											"9. Which Directive Principle encourages the State to promote 'International Peace and Security'?",
+											"11. Who is known as the 'Father of the Indian Constitution'?"];
+
+	namespace.CWP_QUESTIONS_DOWN =   [     "2. How many Fundamental Duties are listed in the Constitution of India?",
+		                                  "4. What is the duty to 'Promote Harmony'?",
+		                                  "5. What is the duty to 'Safeguard Public Property'?",
+		                                  "10. What is the right that ensures 'Eequality of Opportunity' in public employment?",
+										  "12. Which Schedule of the Indian Constitution lists the 'Languages recognized by the Constitution'?",
+										  "13. Which Fundamental Right ensures protection against 'self-incrimination'?"];
 
 	namespace.resetUserInputArray = function() {
 
@@ -373,9 +351,9 @@ var cwp_nmsp = {};
 		inputButtonFinish.type = "button";
 		var inputButtonClear = inputButtonFinish.cloneNode(false);
 		inputButtonFinish.setAttribute("id", "cwp_b_finish");
-		inputButtonFinish.setAttribute("value", "Check Solution/Finish");
+		inputButtonFinish.setAttribute("value", "Check Solution");
 		inputButtonClear.setAttribute("id", "cwp_b_clear");
-		inputButtonClear.setAttribute("value", "Clear All/Reset");
+		inputButtonClear.setAttribute("value", "Clear All");
 		docFragButton.appendChild(inputButtonFinish);
 		docFragButton.appendChild(inputButtonClear);
 		buttonDiv.appendChild(docFragButton);
